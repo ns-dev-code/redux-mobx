3c2509fb0b345c731c137cac478abbca09bb5f78
Nitin <nitin.singh7570@gmail.com>
2020-10-29 08:58:43 +0530

    Initial Commit for redux-Mobx

---
+++ index.js
@@ -0,0 +1,17 @@
+const {
+    createStore,
+    combineReducers,
+    compose,
+    bindActionCreators,
+    applyMiddleware
+} = require('redux')
+
+
+const makeLouder = string => string.toUpperCase();
+const repeatThreeTimes = string => string.repeat(3);
+const embolden = string => string.bold();
+
+const makeLouderAndBoldAndRepeatThreeTimes = compose(
+    embolden, repeatThreeTimes, makeLouder
+)
+console.log(makeLouderAndBoldAndRepeatThreeTimes('hello'))
